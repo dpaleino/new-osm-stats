c76928b189bfc4c5c5aacaa9a6969a2a2d1a88d3
David Paleino <dapal@debian.org>
2010-09-06 11:44:35 +0200

    Use gnuplot to generate graphs

--- plot.py
+++ plot.py
@@ -1,50 +1,62 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from glob import iglob
+from glob import glob
 import cjson
 import Gnuplot
+import tempfile
+import os
 from collections import defaultdict
 
 graphs_path = 'graphs/'
 
-def plot(xcoords, ycoords, file):
-    print xcoords, ycoords
-    print zip(*ycoords)
-    import matplotlib.pyplot as plt
-    import matplotlib.dates as dts
-    from datetime import datetime
-    
-    years = dts.YearLocator()
-    months = dts.MonthLocator()
-    yearsFmt = dts.DateFormatter('%Y')
-    
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    styles = ['bo-', 'kx-', 'y+-', 'go-', 'rx-']
-    i = 0
-    while i < len(zip(*ycoords)):
-        l = zip(*ycoords)[i]
-        ax.plot_date(map(lambda x: dts.date2num(datetime.strptime(x, '%Y%m%d')), xcoords), zip(*l)[1], styles[i], label=zip(*l)[0])
-        i += 1
-#    ax.xaxis.set_major_locator(years)
-#    ax.xaxis.set_major_formatter(yearsFmt)
-#    ax.xaxis.set_minor_locator(months)
-    ax.xaxis.set_major_locator(months)
-    
-    fig.autofmt_xdate()
-    fig.suptitle(file)
-    plt.savefig('%s/%s.png' % (graphs_path, file))
-    plt.close()
+class Graph():
+    def __init__(self, filename, title=None):
+        self.file = filename
+        self.title = title if title else self.file
+        self.user = False
+        self.tmp = []
+
+        self.g = Gnuplot.Gnuplot(debug=1)
+        self.g.title(self.title)
+        self.g("set style data linespoints")
+        self.g("set output '%s.svg'" % os.path.join(graphs_path, self.file))
+        self.g("set terminal svg")
+        self.g("set xdata time")
+        self.g("set timefmt '%Y%m%d'")
+        self.g("set format x '%m/%y'")
+
+    def add_line(self, user, xcoords, ycoords):
+        tmpfile = tempfile.mkstemp()[1]
+        self.tmp.append((user, tmpfile))
+
+        xcoords = map(lambda x: x.split('T')[0], xcoords)
+        print xcoords, ycoords
+
+        f = open(tmpfile, 'w')
+        for x, y in zip(xcoords, ycoords):
+            f.write('%(x)s %(y)s\n' % locals())
+            self.g("set label '%(y)s' at '%(x)s',%(y)s" % locals())
+        f.close()
+
+    def plot(self):
+        l = []
+        for user, tmpfile in self.tmp:
+            l.append("'%s' using 1:2 title '%s'" % (tmpfile, user))
+        self.g("plot " + ', '.join(l))
+        self.g.close()
+        for user, tmpfile in self.tmp:
+            os.unlink(tmpfile)
+
 
 search_tags = ["highway=bus_stop", "addr:housenumber=*"]
 search_users = ["David Paleino", "trimoto", "Gianfra"] #, "tosky"]
 
 xcoords = []
-counts = defaultdict(dict)
+counts = []
 ycoords = defaultdict(list)
 
-for i in iglob('json/sicilia_*.json'):
+for i in sorted(glob('json/italy_*.json')):
     load = cjson.decode(open(i).readline())
     timestamp = load[0]
     nodes = load[1]
@@ -55,36 +67,40 @@ for i in iglob('json/sicilia_*.json'):
     xcoords.append(timestamp.split('T')[0])
 
     l = [nodes, ways, rels]
+    counts.append([])
     for d in l:
         tmpcount = 0
         for c in d:
-            tmpcount += d[c]
+            tmpcount += int(d[c])
 
-        if l.index(d) == 0:
-            counts[timestamp]["nodes"] = tmpcount
-        elif l.index(d) == 1:
-            counts[timestamp]["ways"] = tmpcount
-        elif l.index(d) == 2:
-            counts[timestamp]["rels"] = tmpcount
+        counts[-1].append(tmpcount)
+#        if l.index(d) == 0:
+#            counts[timestamp]["nodes"] = tmpcount
+#        elif l.index(d) == 1:
+#            counts[timestamp]["ways"] = tmpcount
+#        elif l.index(d) == 2:
+#            counts[timestamp]["rels"] = tmpcount
 
     for tag in search_tags:
         key, val = tag.split('=', 1)
         ydate = []
         for user in search_users:
             try:
-                ydate.append((user, tags[key][val][user]))
-                print "%s: %s" % (user, tags[key][val][user])
+                count = tags[key][val][user]
             except KeyError:
-                print "%s: %s" % (user, "0")
+                count = 0
+            ydate.append((user, count))
         ycoords[tag].append(ydate)
+    del l
 
-print counts
+titles = ['Nodes', 'Ways', 'Relations']
+for t in titles:
+    graph = Graph(t)
+    graph.add_line(t, xcoords, zip(*counts)[titles.index(t)])
+    graph.plot()
+
 for tag in search_tags:
-    plot(xcoords, ycoords[tag], tag)
-
-#g = Gnuplot.Gnuplot(debug=1)
-#g.title("A simple example")
-#g("set style data linespoints")
-#g("set output 'foo.png'")
-#g("set terminal png")
-#g.plot([[0,1.1], [1,5.8], [2,3.3], [3,4.2]])
+    graph = Graph(tag)
+    for user in zip(*ycoords[tag]):
+        graph.add_line(zip(*user)[0][0], xcoords, zip(*user)[1])
+    graph.plot()
