4f7492981c0675c46538e738b7b0d6e7061d5f70
David Paleino <dapal@debian.org>
2010-09-09 18:18:47 +0200

    Fix Francesco's script to work with the improved fast_stats.py

--- launch_import.sh
+++ launch_import.sh
@@ -7,8 +7,5 @@ for i in `ls ${DUMPDIR}/*.bz2.*`; do
    echo bunzip ${i}
    echo ./fast_stats ${i:56:28}.out
    echo rm ${i:56:28}.out
-   data=$(date +%Y%m%d)
-   echo "$data -> ${i:70:8}"
-   echo mv json/$data.json json/${i:70:8}.json
    read;
 done
