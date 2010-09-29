e194bfb713f86fa672d6afa1a94dee61b893d0b1
David Paleino <dapal@debian.org>
2010-09-30 01:45:39 +0200

    Complete split+full tags, and relative bugs (fixes #4)

--- fast_stats.py
+++ fast_stats.py
@@ -14,6 +14,7 @@ from helpers import *
 ### configuration
 html_path = "html/"
 jsons_path = "json/"
+maxsplit = 100
 
 ### logging
 log = logging.getLogger('stats')
@@ -178,11 +179,11 @@ def enumerate_tags(tags):
     for key in tags:
         log.debug("Sorting users for key %s" % key)
         enum[key] = myenum(tags[key])
-        enum2[key] = myenum(tags[key])
+        enum2[key] = myenum(tags[key], split=maxsplit)
 
     return [enum, enum2]
 
-def calculate_positions(prefix, date, nodes, ways, rels, enum):
+def calculate_positions(prefix, date, nodes, ways, rels, tags):
     log.info("Calculating positions")
     try:
         primitives_positions, tags_positions = cjson.decode(open('json/%s_positions.json' % prefix).readline())
@@ -198,11 +199,11 @@ def calculate_positions(prefix, date, nodes, ways, rels, enum):
         for user in enumerate(mysort(p[1])):
             primitives_positions[date][p[0]].append(user[1][0])
 
-    for tag in enum:
+    for tag in tags:
         log.debug("Calculating positions for key %s" % tag)
-        for val in enum[tag]:
-            for user in enum[tag][val]:
-                tags_positions[date][tag][val].append(user[1][0])
+        for val in tags[tag]:
+            for user in mysort(tags[tag][val]):
+                tags_positions[date][tag][val].append(user[0])
 
     return [primitives_positions, tags_positions]
 
@@ -216,46 +217,67 @@ def save_jsons(prefix, l, pos):
     f.write(cjson.encode(pos))
     f.close()
 
-def render_template(prefix, date, nodes, ways, rels, tags, positions):
+def render_template(prefix, date, nodes, ways, rels, fulltags, splittags, positions):
     log.info("Rendering HTML")
 
-    log.debug("Rendering primitives pages")
-    tmpl = template(open("views/statistiche.tmpl"))
-    stream = tmpl.generate(
+    log.debug("Rendering primitives pages (%s)" % date)
+
+    for t in [(None, fulltags), (maxsplit, splittags)]:
+        tmpl = template(open("views/statistiche.tmpl"))
+        stream = tmpl.generate(
                        date=date,
-                       nodes=enumerate(mysort(nodes)),
-                       ways=enumerate(mysort(ways)),
-                       relations=enumerate(mysort(rels)),
+                       nodes=enumerate(mysort(nodes, split=t[0])),
+                       ways=enumerate(mysort(ways, split=t[0])),
+                       relations=enumerate(mysort(rels, split=t[0])),
                        prefix=prefix,
-                       tags=sorted(tags.keys()),
-                       files=[sanitize(x) for x in sorted(tags.keys())],
+                       tags=sorted(fulltags.keys()),
+                       files=[sanitize(x) for x in sorted(fulltags.keys())],
                        pos=positions_changed(positions[0]),
-    )
-
-    f = open(os.path.join(html_path, '%s_stats.html' % prefix), "w")
-    f.write(stream.render("xhtml"))
-    f.close()
-
-    pos = positions_changed(positions[1])
-    for key in sorted(tags.keys()):
-        log.debug("Rendering pages for %s=*", key)
-        stream = template(open('views/key.tmpl')).generate(
-            date=date,
-            prefix=prefix,
-            key=key,
-            vals=tags[key],
-            pos=pos[key],
         )
-        f = open(os.path.join(html_path, '%s_%s.html' % (prefix, sanitize(key))), 'w')
-        f.write(stream.render('xhtml'))
+
+        if not t[0]:
+            # i.e. if t[0] is None, i.e. we're doing the fulltags
+            f = open(os.path.join(html_path, '%s_stats_full.html' % prefix), "w")
+        else:
+            # we're rendering splittags
+            f = open(os.path.join(html_path, '%s_stats.html' % prefix), "w")
+        f.write(stream.render("xhtml"))
         f.close()
+
+        for key in sorted(t[1].keys()):
+            log.debug("Rendering pages for %s=* (%s)" % (key, date))
+            stream = template(open('views/key.tmpl')).generate(
+                date=date,
+                prefix=prefix,
+                key=key,
+                vals=t[1][key],
+                pos=positions_changed(positions[1])[key],
+                split=t[0],
+            )
+            if not t[0]:
+                # i.e. if t[0] is None, i.e. we're doing the fulltags
+                f = open(os.path.join(html_path, '%s_%s_full.html' % (prefix, sanitize(key))), 'w')
+            else:
+                # we're rendering splittags
+                f = open(os.path.join(html_path, '%s_%s.html' % (prefix, sanitize(key))), 'w')
+            f.write(stream.render('xhtml'))
+            f.close()
 
 def main(prefix, date, filename):
     nodes, ways, rels, tags = parse(filename)
-    enum, enum2 = enumerate_tags(tags)
-    positions = calculate_positions(prefix, date, nodes, ways, rels, enum)
+    positions = calculate_positions(prefix, date, nodes, ways, rels, tags)
     save_jsons(prefix, [date, nodes, ways, rels, tags], positions)
-    render_template(prefix, date, nodes, ways, rels, enum2, positions)
+
+    # render the last date, we don't support rendering intermediate dates (yet,
+    # see commented code in helpers.py:positions_changed()
+    secondlast, last = get_last_dates(positions[0])
+
+    log.debug('Loading data for %s' % last)
+    json = open(os.path.join(jsons_path, '%s_%s.json' % (prefix, last)))
+    timestamp, nodes, ways, rels, tags = cjson.decode(json.readline())
+
+    enum, enum2 = enumerate_tags(tags)
+    render_template(prefix, last, nodes, ways, rels, enum, enum2, positions)
     log.info("Program ended.")
 
 if __name__ == '__main__':
--- helpers.py
+++ helpers.py
@@ -3,14 +3,21 @@
 
 from operator import itemgetter
 from collections import defaultdict
+from copy import deepcopy
 
-def mysort(d):
-    return sorted(d.items(), key=itemgetter(1), reverse=True)
+def mysort(d, split=None):
+    if not split:
+        return sorted(d.items(), key=itemgetter(1), reverse=True)
+    else:
+        return sorted(d.items(), key=itemgetter(1), reverse=True)[:split]
 
-def myenum(d):
+def myenum(d, split=None):
     enum = {}
     for key in d:
-        enum[key] = enumerate(mysort(d[key]))
+        if not split:
+            enum[key] = enumerate(mysort(d[key]))
+        else:
+            enum[key] = enumerate(mysort(d[key], split=split))
     return enum
 
 def key_wildcard(key, ret=None):
@@ -24,14 +31,20 @@ def key_wildcard(key, ret=None):
         key_wildcard(':'.join(partial), ret)
     return set(ret)
 
-def positions_changed(positions):
-    ret = defaultdict(dict)
+def get_last_dates(positions):
     dates = sorted(positions.keys())
     last = dates[-1:][0]
     try:
         secondlast = dates[-2:-1][0]
     except IndexError:
         secondlast = last
+    return secondlast, last
+
+def positions_changed(positions):
+#    ret = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+    ret = defaultdict(dict)
+    secondlast, last = get_last_dates(positions)
+
     for key in positions[last]:
         if key in ['Nodi', 'Ways', 'Relazioni']:
             for user in positions[last][key]:
@@ -43,8 +56,28 @@ def positions_changed(positions):
                     pos_old = pos_new
                 ret[key][user] = int(pos_old - pos_new)
         else:
+            # FIXME: this will be useful when supporting intermediate
+            # renderings
+#            # Try to sanitize keys
+#            # TODO: this should REALLY be fixed.
+#            oldkeys = positions[secondlast][key].keys()
+#            newkeys = positions[last][key].keys()
+#            keyscopy = deepcopy(newkeys)
+#
+#            for t in [(oldkeys, newkeys), (keyscopy, oldkeys)]:
+#                for v in t[0]:
+#                    try:
+#                        t[1].remove(v)
+#                    except ValueError:
+#                        pass
+#            if oldkeys != newkeys:
+#                # oldkeys contains keys present in positions[secondlast] but not in [last]
+#                # newkeys contains keys present in positions[last] but not in [secondlast]
+#                print repr(oldkeys), repr(newkeys), repr(keyscopy)
+#                pass
+
             for val in positions[last][key]:
-                tmp = defaultdict(dict)
+                tmp = defaultdict(int)
                 for user in positions[last][key][val]:
                     pos_new = positions[last][key][val].index(user)
                     try:
