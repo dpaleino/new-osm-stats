ffbde049e93e8a7eda37f2ac441eb011159cbb14
David Paleino <dapal@debian.org>
2010-11-01 20:17:46 +0100

    Generate translation template

--- update-po.sh
+++ update-po.sh
@@ -7,11 +7,20 @@ pybabel extract -F po/babel.cfg . -o po/osmstats.pot \
 sed -i "s/PROJECT/osmstats/g" po/osmstats.pot
 sed -i "s/VERSION/0.1/g" po/osmstats.pot
 
+echo -n '<p class="langs">' > views/l10n.tmpl
+
 for trans in $(ls po/*.po); do
 	msgmerge --update $trans po/osmstats.pot
 	name=$(basename $trans .po)
 	[ -d po/$name/LC_MESSAGES ] || mkdir -p po/$name/LC_MESSAGES
 	msgfmt $trans -o po/$name/LC_MESSAGES/messages.mo
+
+	lang=$(cat $trans | grep Team | sed -e 's/"Language-Team: \(.*\)\\n"/\1/')
+	echo -n "<a href=\"/lang/${name}\">" >> views/l10n.tmpl
+	echo -n "<abbr title=\"${lang}\">${name}</abbr>" >> views/l10n.tmpl
+	echo -n '</a> ' >> views/l10n.tmpl
 done
+
+echo '</p>' >> views/l10n.tmpl
 
 msgen po/osmstats.pot | msgmerge --update po/en.po -
